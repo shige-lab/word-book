bf18080b5941dc44f16ed3bbe541096676961513
shige-lab <shige.bask@gmail.com>
2024-09-13 20:27:37 +1000

    feat: define types

---
+++ mobile/src/types/navigator/type.ts
@@ -0,0 +1,35 @@
+export interface Category {
+  id: number;
+  name: string;
+  order_index: number;
+  words?: Word[];
+  childrenLength?: number;
+}
+
+export interface Proficiency {
+  id: number;
+  name: string;
+  order_index: number;
+}
+
+export interface Frequency {
+  id: number;
+  name: string;
+  order_index: number;
+}
+
+export interface Word {
+  id: number;
+  word: string;
+  meaning: string;
+  category_id: number;
+  proficiency: Proficiency;
+  proficiency_id: number;
+  frequency: Frequency;
+  frequency_id: number;
+  note: string;
+  example1: string;
+  example2: string;
+  example3: string;
+  image?: string;
+}
