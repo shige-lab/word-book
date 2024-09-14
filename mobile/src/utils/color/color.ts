e9c166a59d364b0b93aa9ea79976ad6998bf0c1a
shige-lab <shige.bask@gmail.com>
2024-09-14 23:51:38 +1000

    fix: small color change

--- mobile/src/utils/color/color.ts
+++ mobile/src/utils/color/color.ts
@@ -1,4 +1,4 @@
-export const headerColor = '#90cdf4';
+export const headerColor = '#4299e1';
 
 export const darkColor = {
   base: 'black',
@@ -10,4 +10,9 @@ export const whiteColor = {
   base: 'white',
   base1: '#edf2f7',
   base2: 'cbd5e0',
+};
+
+export const borderBottom = {
+  borderBottomWidth: 0.5,
+  borderBottomColor: 'gray300',
 };
