4c168ac27a49300c81b47b090e1856b91bc15cea
shige-lab <shige.bask@gmail.com>
2024-09-14 22:26:31 +1000

    feat: create useColor

---
+++ mobile/src/hooks/common/useColor.ts
@@ -0,0 +1,10 @@
+import {Appearance} from 'react-native';
+import {darkColor, whiteColor} from '../../utils/color/color';
+
+export const useColor = () => {
+  const isDarkMode = Appearance.getColorScheme() === 'dark';
+  return {
+    primary: isDarkMode ? 'white' : 'black',
+    baseColor: isDarkMode ? darkColor : whiteColor,
+  };
+};
