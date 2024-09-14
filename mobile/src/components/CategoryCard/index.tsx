ba02acc6445afbf909c76939135dbc38d2eb1a21
shige-lab <shige.bask@gmail.com>
2024-09-14 23:51:29 +1000

    fix: add navigation on category card

--- mobile/src/components/CategoryCard/index.tsx
+++ mobile/src/components/CategoryCard/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import {Div, Text} from 'react-native-magnus';
 import {Category} from '../../types/navigator/type';
 import {TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {navigationProp} from '../../types/navigator/RouteProps';
 
 const CategoryCard = ({category}: {category: Category}) => {
+  const navigation = useNavigation<navigationProp>();
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate('Category', {
+          id: category.id,
+        })
+      }>
       <Div h={40} py="sm" justifyContent="center">
         <Text fontSize={16} fontWeight="bold">
           {category.name}
