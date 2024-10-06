2ae7d66733a2b896a628ae5aa34f36605faa6aef
shige-lab <shige.bask@gmail.com>
2024-10-06 18:16:39 +1000

    feat: create useSound

---
+++ mobile/src/hooks/common/useSound.ts
@@ -0,0 +1,46 @@
+import {useState} from 'react';
+import Sound from 'react-native-sound';
+
+export const useSound = () => {
+  const [sound, setSound] = useState<Sound>();
+
+  const stopSound = () => {
+    if (sound) {
+      sound.stop(() => {
+        console.log('Sound stopped');
+      });
+    }
+  };
+
+  const playSound = (url: string) => {
+    Sound.setCategory('Playback');
+    // If there is already a sound playing, stop it before starting a new one
+    if (sound) {
+      sound.stop(() => {
+        console.log('Sound stopped');
+      });
+    }
+
+    // Load the sound file from a remote URL or local path
+    console.log('---url---', url);
+
+    const newSound = new Sound(url, undefined, error => {
+      if (error) {
+        console.log('Failed to load the sound', error);
+        return;
+      }
+      // Play the sound once it is loaded
+      newSound.play(success => {
+        if (success) {
+          console.log('Successfully finished playing');
+        } else {
+          console.log('Playback failed due to audio decoding errors');
+        }
+      });
+    });
+    // Release the sound resource when playback is complete
+    newSound.release();
+    setSound(newSound);
+  };
+  return {playSound, stopSound};
+};
