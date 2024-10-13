9e8306ae602f0cd0820b021a54540b98662cf256
shige-lab <shige.bask@gmail.com>
2024-10-13 23:27:11 +1000

    feat: create BottomActionButton component

---
+++ mobile/src/components/Common/BottomActionButton/index.tsx
@@ -0,0 +1,69 @@
+import React, {useEffect, useMemo, useState} from 'react';
+import {Button, Div, ScrollDiv, Text} from 'react-native-magnus';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {Dimensions} from 'react-native';
+import {useColor} from '../../../hooks/common/useColor';
+
+interface ActionProps {
+  label: 'Delete' | 'Move';
+  onPress: () => void;
+  disabled?: boolean;
+}
+
+export interface BottomActionButtonProps {
+  onCancel: () => void;
+  actions: ActionProps[];
+}
+
+const BottomActionButton: React.FC<BottomActionButtonProps> = ({
+  actions,
+  onCancel,
+}) => {
+  const insets = useSafeAreaInsets();
+  const {width} = Dimensions.get('window');
+  const {baseColor, primary} = useColor();
+
+  const buttonWidth = useMemo(() => {
+    const len = actions.length + 1;
+    console.log('len', width, (width - 12 * (len + 2)) / len);
+
+    return (width - 12 * (len + 1)) / len;
+  }, [actions.length, width]);
+
+  return (
+    <Div
+      mt="auto"
+      // position="absolute"
+      // bottom={0}
+      borderBottomWidth={1}
+      pb={insets.bottom + 10}
+      bg={baseColor.base}
+      w="100%"
+      p={12}
+      flexDir="row"
+      justifyContent="space-between"
+      alignItems="center">
+      <Button
+        w={buttonWidth}
+        bg="white"
+        color={primary}
+        borderWidth={1}
+        borderColor="gray300"
+        onPress={onCancel}>
+        Cancel
+      </Button>
+      {actions.map((action, index) => (
+        <Button
+          key={index}
+          w={buttonWidth}
+          bg={action.label === 'Delete' ? 'red600' : 'blue600'}
+          disabled={action.disabled}
+          onPress={action.onPress}>
+          {action.label}
+        </Button>
+      ))}
+    </Div>
+  );
+};
+
+export default BottomActionButton;
